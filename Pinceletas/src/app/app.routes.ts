3f38e5a942d4b271e422d80d03d4133f47ca0b17
Agustín Ivan, Olivera <114453@tecnicatura.frc.utn.edu.ar>
2025-09-29 17:23:45 -0300

    Front inicial

---
+++ Pinceletas/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Routes } from '@angular/router';
+
+
+export const routes: Routes = [
+    {
+    path: 'productlist',
+    loadComponent: () =>
+      import('./modules/commerce/product-list/product-list.component')
+        .then(m => m.ProductListComponent)
+  },
+  {
+    path: 'productdetail',
+    loadComponent: () =>
+      import('./modules/commerce/product-detail/product-detail.component')
+        .then(m => m.ProductDetailComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'productlist',
+    pathMatch: 'full'
+  }
+];
---
+++ Pinceletas/src/app/enviroment/user-auth-enviroment.ts
@@ -0,0 +1,6 @@
+export const userAuthEnviroment = {
+    // apiUsuarios : 'http://localhost:8081/usuarios',
+    // apiPosteos : 'http://localhost:8081/posteos',
+
+    
+  };
